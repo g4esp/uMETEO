18bb43d1b27e6c7d420052f52450b1ddd8cf2e1c
g4esp <gianniesp@outlook.com>
2022-09-04 01:23:37 +0200

    fix api call

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import Today from "./components/Today"
 import Tomorrow from "./components/Tomorrow"
 import { Transition } from "@headlessui/react"
 
-const appid = import.meta.env.VITE_APP_ID
+// const appid = import.meta.env.VITE_APP_ID
+const appid = "9d80cddf8c01893b2fc8d8a417227284"
+
 const api = `https://api.openweathermap.org/data/2.5/`
 const params = `lang=it&units=metric`
 
