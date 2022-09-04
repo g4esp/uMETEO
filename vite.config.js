751b62be531f9ed14d2af1a60946d8df423c4274
g4esp <gianniesp@outlook.com>
2022-09-04 17:15:31 +0200

    fix path to icons

--- vite.config.js
+++ vite.config.js
@@ -15,13 +15,13 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'src/pwa-192x192.png',
-            sizes: '192x192',
+            src: '/src/pwa-512x512.png',
+            sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'src/pwa-512x512.png',
-            sizes: '512x512',
+            src: '/src/pwa-192x192.png',
+            sizes: '192x192',
             type: 'image/png'
           }
         ]
